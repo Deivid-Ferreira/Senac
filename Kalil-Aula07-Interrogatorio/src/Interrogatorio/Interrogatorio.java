8f58d373423c79d20fb43a571c4317f685eafcca
Dimy <118582229+Deivid-Ferreira@users.noreply.github.com>
2024-04-11 20:33:41 -0300

    Add files via upload

---
+++ Kalil-Aula07-Interrogatorio/src/Interrogatorio/Interrogatorio.java
@@ -0,0 +1,30 @@
+package Interrogatorio;
+
+public class Interrogatorio {
+
+	public static void main(String[] args) {
+
+		boolean resp1 = false;
+		boolean resp2 = false;
+		boolean resp3 = false;
+		boolean resp4 = false;
+		boolean resp5 = true;
+
+		System.out.println("Responda as perguntas\n------------------------------------");
+
+		System.out.println("Telefonou para a vítima?");
+
+		System.out.println("Esteve no local do crime?");
+
+		System.out.println("Mora perto da vítima?");
+
+		System.out.println("Devia para a vítima?");
+
+		System.out.println("Já trabalhou com a vítima?");
+
+		Respostas respostas = new Respostas(resp1, resp2, resp3, resp4, resp5);
+		System.out.println(respostas.veredito(respostas.contadorDeRespostas()));
+       
+	}
+
+}
